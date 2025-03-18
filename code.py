5107c62951ee4d3795b2a484d5a35ea145f2d4af
Manish Singh <2701msingh@gmail.com>
2025-03-18 22:06:34 +0530

    updated code
    it is updated code .

--- code.py
+++ code.py
@@ -8,7 +8,6 @@ import webbrowser
 import json
 API_KEY = 'bd8cf8c8e762bbdaa8de1c91d2deddf8'
 districts = ['Dehradun', 'Almora', 'Nainital', 'Pauri', 'Rudraprayag', 'Tehri', 'UdhamSinghNagar', 'Pithoragarh', 'Bageshwar', 'Haridwar', 'Chamoli', 'Champawat']
-
 # File paths
 data_file = 'crop_data.json'
 
